b62ebc5e8d54279c0d203d68a3db2e2062a96988
Mike Gerdts <mike.gerdts@joyent.com>
2019-03-08 23:46:16 +0000

    implement SCFHandle

--- src/lib.rs
+++ src/lib.rs
@@ -43,19 +43,48 @@ impl Error for SMFError {
     }
 }
 
+struct SCFHandle {
+    handle: *mut scf_handle_t,
+    bound: bool,
+}
+
+impl SCFHandle {
+    fn open() -> Result<SCFHandle> {
+        let hdl = unsafe { scf_handle_create(SCF_VERSION as u64) };
+        if unsafe { scf_handle_bind(hdl) } != 0 {
+            return Err(SMFError::new());
+        }
+        Ok(SCFHandle {
+            handle: hdl,
+            bound: true,
+        })
+    }
+
+    fn close(&mut self) {
+        if self.bound {
+            unsafe {
+                scf_handle_unbind(self.handle);
+                scf_handle_destroy(self.handle);
+            }
+            self.bound = false;
+        }
+    }
+}
+
+impl Drop for SCFHandle {
+    fn drop(&mut self) {
+        self.close();
+    }
+}
+
 pub fn my_fmri() -> Result<String> {
     let len = unsafe { scf_limit(SCF_LIMIT_MAX_FMRI_LENGTH as u32) as usize };
     let ptr = CString::new(" ".repeat(len)).unwrap().into_raw();
 
+    let hdl = SCFHandle::open()?;
+
     unsafe {
-        let hdl = scf_handle_create(SCF_VERSION as u64);
-        if scf_handle_bind(hdl) != 0 {
-            scf_handle_destroy(hdl);
-            return Err(SMFError::new());
-        }
-        let need = scf_myname(hdl, ptr, len);
-        scf_handle_unbind(hdl);
-        scf_handle_destroy(hdl);
+        let need = scf_myname(hdl.handle, ptr, len);
         if need == -1 {
             return Err(SMFError::new());
         }
