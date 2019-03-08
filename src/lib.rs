43a5c23479aa7aea1e1ef14c52939660d030d837
Mike Gerdts <mike.gerdts@joyent.com>
2019-03-08 15:37:14 +0000

    double after my_fmri() #1

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,29 @@
 #![allow(dead_code)]    // To hush ffi warnings, must be a better way
 extern crate libc;
 use crate::ffi::*;
-use libc::c_char;
 use std::ffi::CString;
 
 mod ffi;
 
 pub fn my_fmri() -> Option<String> {
     let len = unsafe { scf_limit(SCF_LIMIT_MAX_FMRI_LENGTH as u32) as usize };
-    let mut buf: Vec<c_char> = Vec::with_capacity(len);
-    let ptr = buf.as_mut_ptr() as *mut libc::c_char;
+    let ptr = CString::new(format!("{:len$}", " ", len = len)).unwrap()
+        .into_raw();
 
-    let hdl;
     unsafe {
-        hdl = scf_handle_create(SCF_VERSION as u64);
+        let hdl = scf_handle_create(SCF_VERSION as u64);
         if scf_handle_bind(hdl) != 0 {
             scf_handle_destroy(hdl);
             return None;
         }
-    }
-
-    let need;
-    unsafe {
-        need = scf_myname(hdl, ptr, len);
+        let need = scf_myname(hdl, ptr, len);
         scf_handle_unbind(hdl);
         scf_handle_destroy(hdl);
+        if need == -1 {
+            return None;
+        }
+        Some(CString::from_raw(ptr).into_string().unwrap())
     }
-    if need == -1 {
-        return None;
-    }
-    let cstring = unsafe { CString::from_raw(ptr).into_string().unwrap() };
-    println!("Before return cstring {}", cstring);
-    Some(cstring)
 }
 
 #[cfg(test)]
